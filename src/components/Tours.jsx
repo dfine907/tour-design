28a3781ff05ed98e37bc7b7c584eac23b9395dd5
Dina <dinagiacobbi@gmail.com>
2025-01-07 11:14:12 -0900

    Building Tour component

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tour from './Tour'
 
 const Tours = ({ tours }) => {
   return (
@@ -7,6 +8,13 @@ const Tours = ({ tours }) => {
         <div className="title">
           <h2>Our tours</h2>
           <div className="title-underline"></div>
+        </div>
+
+        <div className="tours">
+          {tours.map((tour) => {
+            
+            return <Tour key={tour.id} {...tour} />
+          })}
         </div>
       </section>
     </>
