35c3dabafa24d3ae9e58284e84d3eb73e769b8de
Dina <dinagiacobbi@gmail.com>
2025-01-08 07:55:17 -0900

    Adding Show More text feature

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+
+const [readMore, setReadMore]= useState(false)
+// console.log(info.substring(0, 10))
+
   return (
     <>
       <article className="single-tour">
@@ -8,9 +13,16 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
         <span className="tour-price"> ${price}</span>
         <div className="tour-info">
           <h5>{name}</h5>
-          <p>{info}</p>
+          <p>{readMore ? info : `${info.substring(0,150)} ... ` }
+
+
+            <button type='button' className="info-btn" onClick={()=> setReadMore(!readMore)}>
+              {readMore ? ` Show less` : ` Read more`}
+            </button>
+          </p>
         </div>
         <button
+         type='button'
           className="btn btn-block delete-btn"
           onClick={() => removeTour(id)}
         >
